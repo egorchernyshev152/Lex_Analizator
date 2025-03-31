90993a4bcd467d75569b8a26546463dc06ecd6a6
alexis1337 <talanov-a-n@mail.ru>
2025-03-31 11:47:19 +0300

    чтение из файла

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
 import lexer.Lexer;
 import lexer.Token;
 import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String testProgram =
-                "(* Sample program *)\n" +
-                        "begin\n" +
-                        "  x := 10;\n" +
-                        "  y := 20.5;\n" +
-                        "  if (x < y) writeln(\"x is less than y\") else writeln(\"x is greater or equal\");\n" +
-                        "end\n";
+        // Проверяем, передан ли аргумент (путь к файлу)
+        if (args.length == 0) {
+            System.err.println("Ошибка: Укажите путь к файлу с исходным кодом.");
+            System.err.println("Пример: java Main program.txt");
+            return;
+        }
+
+        String filePath = args[0];
+        String testProgram;
+
+        try {
+            // Читаем весь файл в строку
+            testProgram = Files.readString(Paths.get(filePath));
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+            return;
+        }
 
         Lexer lexer = new Lexer(testProgram);
         List<Token> tokens = lexer.tokenize();
